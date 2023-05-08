9c28f96fbe4da3ee8ad3b61c6c67d7f967693796
Ariel Martinez <[200~91949764+arielviale@users.noreply.github.com~>
2023-05-08 11:11:38 -0300

    array vacios.

--- micodigo.js
+++ micodigo.js
@@ -1,5 +1,9 @@
 var minumero = 11 % 3;
 //minumero += 5; //agregue el operador suma y multiplicacion.
-var miarray = [64, 32];
-miarray[1] = miarray[1] + 10;
-alert(miarray[1]);
+// var miarray = [64, 32];
+// miarray[1] = miarray[1] + 10;
+// alert(miarray[1]); //array multidimensionales.
+
+var miarray = [2, 4, 5];
+miarray = [];
+alert(miarray);//array vacio.
