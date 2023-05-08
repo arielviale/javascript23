7a736dc34d7e2473efac6710747f07b5d9aeaf2d
Ariel Martinez <[200~91949764+arielviale@users.noreply.github.com~>
2023-05-08 11:51:51 -0300

    uso de switch

--- micodigo.js
+++ micodigo.js
@@ -1,9 +1,26 @@
-// var mivariable = 9;
-// if(!(mivariable < 10)) {
+// var mivariable = 8;
+// if(mivariable < 10) {
 //     alert("El numero es menor que 10");
+// } else {
+//     alert("el numero es igual o mayor que 10");
 // }
-var inteligente = true;
-var edad = 19;
-if (edad < 21 && inteligente) {
-    alert("Juan esta autorizado");
+// var inteligente = "SI";
+// var edad = 19;
+// if (edad < 21 && inteligente) {
+//     alert("Juan esta autorizado");
+// }
+var mivariable = 3;
+switch(mivariable) {
+    case 5:
+        alert("El numero es 5");
+        break;
+    case 8:
+        alert("El numero es 8");
+        break;
+    case 10:
+        alert("El numero es 10");
+        break;
+    default:
+        alert("El numero es " + mivariable);
+    
 }
